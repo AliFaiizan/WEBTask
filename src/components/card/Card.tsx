f0e8b221bbc91654dd5691d02e720426549a7822
alifaiizan <alifaizanahmad@gmail.com>
2023-07-12 23:38:53 +0200

    card changes

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -17,21 +17,25 @@ type CardProps = {
 const Card: React.FC<CardProps> = ({poster,title,type,year,imdbID}:CardProps) => {
   return (
     <div className={`${styles.card} ${inter.className}`}>
-    <Link href={"/"}>
-      <div>
-        <Image
-          src={poster}
-          width={300}
-          height={400}
-          alt="Card Image"
-          className={styles.cardImage}
-        />
-      </div>
-      <div className={styles.cardContent}>
-        <h2 className={styles.cardTitle}>{title}</h2>
-        <p className={styles.cardRuntime}>{year}</p>
-      </div>
-    </Link>
+      <Link href={"/"}>
+        <div>
+          <Image
+            src={
+              poster === "N/A"
+                ? "/movie-placeholder.jpg"
+                : poster
+            }
+            width={300}
+            height={400}
+            alt="Card Image"
+            className={styles.cardImage}
+          />
+        </div>
+        <div className={styles.cardContent}>
+          <h2 className={styles.cardTitle}>{title}</h2>
+          <p className={styles.cardRuntime}>{year}</p>
+        </div>
+      </Link>
     </div>
   );
 };
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import { Inter } from 'next/font/google';
 
 import styles from './NavBar.module.css'
 import Link from 'next/link';
-import { useActions } from "@/hooks/useActions";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,20 +11,36 @@ type NavBarProps = {}
 
 const NavBar: React.FC<NavBarProps> = () => {
 
+   const [active, setActive] = useState("search");
   
 
   return (
     <nav className={[styles.navbar, inter.className].join(" ")}>
-      <div className={styles.logo}>OMDB</div>
+      <div className={styles.logo}>
+        OM<span>DB</span>
+      </div>
       <ul className={styles.navLinks}>
         <div className={styles.menu}>
-          
-          <li>
-            <Link type="submit" href="/">Search</Link>
-          </li>
-          <li>
-            <Link href="/recommend">Recommendations</Link>
-          </li>
+          <Link type="submit" href="/">
+            <div
+              className={active === "search" ? styles.active : ""}
+              onClick={() => {
+                setActive("search");
+              }}
+            >
+              <li>Search</li>
+            </div>
+          </Link>
+          <Link href="/recommend">
+            <div
+              className={active === "recommend" ? styles.active : ""}
+              onClick={() => {
+                setActive("recommend");
+              }}
+            >
+              <li>Recommendations</li>
+            </div>
+          </Link>
         </div>
       </ul>
     </nav>
